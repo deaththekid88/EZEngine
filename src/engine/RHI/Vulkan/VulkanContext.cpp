53d2d3dc5c4d2186bb54ebfc0b2dfe611a34ef7f
deaththekid <deaththekido8888@gmail.com>
2026-03-09 14:31:38 +0900

    VulkanContext 클래스에 물리적 장치 선택 및 논리적 장치 생성 기능 추가, 큐 패밀리 인덱스 구조체 정의

--- src/engine/RHI/Vulkan/VulkanContext.cpp
+++ src/engine/RHI/Vulkan/VulkanContext.cpp
@@ -5,6 +5,7 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 #include <cstring>
+#include <set>
 
 using namespace EZEngine::Core;
 using namespace EZEngine::Platform;
@@ -63,6 +64,10 @@ namespace EZEngine::RHI
             return false;
         if (!EnumeratePhysicalDevices())
             return false;
+        if (!PickPhysicalDevice())
+            return false;
+        if (!CreateLogicalDevice())
+            return false;
 
         Log("Vulkan context initialized successfully.", LogType::INFO);
         return true;
@@ -70,6 +75,12 @@ namespace EZEngine::RHI
 
     void VulkanContext::Shutdown()
     {
+        if(m_device != VK_NULL_HANDLE)
+        {
+            vkDestroyDevice(m_device, nullptr);
+            m_device = VK_NULL_HANDLE;
+        }
+
         if(m_surface != VK_NULL_HANDLE)
         {
             vkDestroySurfaceKHR(m_instance, m_surface, nullptr);
@@ -189,6 +200,109 @@ namespace EZEngine::RHI
             vkGetPhysicalDeviceProperties(device, &deviceProperties);
             Log(" - " + std::string(deviceProperties.deviceName), LogType::INFO);
         }
+        return true;
+    }
+
+    QueueFamilyIndices VulkanContext::FindQueueFamilies(VkPhysicalDevice device)
+    {
+        QueueFamilyIndices indices;
+
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for(const auto& queueFamily : queueFamilies)
+        {
+            if(queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                indices.graphicsFamily = i;
+
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(device, i, m_surface, &presentSupport);
+            
+            if(presentSupport)
+                indices.presentFamily = i;
+
+            if(indices.IsComplete())
+                break;
+
+            i++;
+        }
+        return indices;
+    }
+
+    bool VulkanContext::PickPhysicalDevice()
+    {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(m_instance, &deviceCount, nullptr);
+        if (deviceCount == 0)
+        {
+            Log("Failed to find GPUs with Vulkan support.", LogType::ERROR);
+            return false;
+        }
+
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(m_instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices)
+        {
+            QueueFamilyIndices indices = FindQueueFamilies(device);
+            if (indices.IsComplete())
+            {
+                m_physicalDevice = device;
+                
+                VkPhysicalDeviceProperties deviceProperties;
+                vkGetPhysicalDeviceProperties(m_physicalDevice, &deviceProperties);
+
+                Log("Selected GPU: " + std::string(deviceProperties.deviceName), LogType::INFO);
+                Log("Graphics Queue Family: " + std::to_string(indices.graphicsFamily.value()), LogType::INFO);
+                Log("Present Queue Family: " + std::to_string(indices.presentFamily.value()), LogType::INFO);
+                return true;
+            }
+        }
+
+        Log("Failed to find a suitable GPU.", LogType::ERROR);
+        return false;
+    }
+
+    bool VulkanContext::CreateLogicalDevice()
+    {
+        QueueFamilyIndices indices = FindQueueFamilies(m_physicalDevice);
+
+        std::vector<VkDeviceQueueCreateInfo> queueCreateInfos;
+        std::set<uint32_t> uniqueQueueFamilies = {indices.graphicsFamily.value(), indices.presentFamily.value()};
+
+        float queuePriority = 1.0f;
+        for (uint32_t queueFamily : uniqueQueueFamilies)
+        {
+            VkDeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+            queueCreateInfo.queueFamilyIndex = queueFamily;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = &queuePriority;
+            queueCreateInfos.push_back(queueCreateInfo);
+        }
+
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        if (vkCreateDevice(m_physicalDevice, &createInfo, nullptr, &m_device) != VK_SUCCESS)
+        {
+            Log("Failed to create logical device.", LogType::ERROR);
+            return false;
+        }
+
+        vkGetDeviceQueue(m_device, indices.graphicsFamily.value(), 0, &m_graphicsQueue);
+        vkGetDeviceQueue(m_device, indices.presentFamily.value(), 0, &m_presentQueue);
+
+        Log("Logical device created.", LogType::INFO);
         return true;
     }
 }
--- src/engine/RHI/Vulkan/VulkanContext.h
+++ src/engine/RHI/Vulkan/VulkanContext.h
@@ -1,9 +1,21 @@
 #pragma once
 #include "engine/Platform/GlfwWindow.h"
 #include <vulkan/vulkan.h>
+#include <optional>
 
 namespace EZEngine::RHI
 {
+    struct QueueFamilyIndices
+    {
+        std::optional<uint32_t> graphicsFamily;
+        std::optional<uint32_t> presentFamily;
+
+        bool IsComplete() const
+        {
+            return graphicsFamily.has_value() && presentFamily.has_value();
+        }
+    };
+
     class VulkanContext
     {
     public:
@@ -16,8 +28,19 @@ namespace EZEngine::RHI
         bool CreateSurface(EZEngine::Platform::GlfwWindow& window);
         bool EnumeratePhysicalDevices();
 
+        bool PickPhysicalDevice();
+        bool CreateLogicalDevice();
+
+        QueueFamilyIndices FindQueueFamilies(VkPhysicalDevice device);
+    
+    private:
         VkInstance m_instance = VK_NULL_HANDLE;
         VkDebugUtilsMessengerEXT m_debugMessenger = VK_NULL_HANDLE;
         VkSurfaceKHR m_surface = VK_NULL_HANDLE;
+        VkPhysicalDevice m_physicalDevice = VK_NULL_HANDLE; 
+        VkDevice m_device = VK_NULL_HANDLE;
+
+        VkQueue m_graphicsQueue = VK_NULL_HANDLE;
+        VkQueue m_presentQueue = VK_NULL_HANDLE;
     };
 }
